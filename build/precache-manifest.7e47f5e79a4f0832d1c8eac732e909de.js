f43057422ce9d7ce14a16362cd0b783740785248
Anna Amidani <an5tash@gmail.com>
2019-01-25 16:12:36 +0000

    Azure deployment

---
+++ build/precache-manifest.7e47f5e79a4f0832d1c8eac732e909de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d699a5a4f49bb3e4c136",
+    "url": "/static/css/main.cf7c2d59.chunk.css"
+  },
+  {
+    "revision": "d699a5a4f49bb3e4c136",
+    "url": "/static/js/main.d699a5a4.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "306cf2892680bdf2d67e8c967be2a392",
+    "url": "/index.html"
+  }
+];
