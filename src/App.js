b4a78337b1026dd785376e5081b96fe561049781
Anna Amidani <an5tash@gmail.com>
2019-01-27 23:08:12 +0000

    fetch data from backend service

--- src/App.js
+++ src/App.js
@@ -3,16 +3,41 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+	this.state = {
+		bookings: [],
+	};
+  }
+
+  componentDidMount() {
+	fetch('https://meetingrooms-booking.azurewebsites.net/bookings/findAll')
+		.then(response => response.json())
+		.then(data => this.setState({ bookings: data }));
+	}
+
   render() {
+	const { bookings } = this.state;
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Awesome app for booking rooms
-          </p>
-        </header>
-      </div>
+	<div className="App">
+	  <header className="App-header">
+	    <h4>Meeting rooms booking App</h4>
+		<img src={logo} className="App-logo" alt="logo" />
+		<p>
+			Bookings list:
+			<ul>
+				{bookings.map(booking =>
+				<li key={booking.objectID}>
+					{booking.roomName}>{booking.roomRef} - from: {booking.from} to: {booking.to}
+				</li>
+				)}
+			</ul>
+		</p>
+	  </header>
+	</div>
     );
   }
 }
