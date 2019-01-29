2030eb621bffd4fb49e0fb58939f66c779b88e3c
Adam Lodowski <adamlodowski@gmail.com>
2019-01-29 13:37:07 +0000

    Implemented Form to submit room availability

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
 
   constructor(props) {
     super(props);
-
-	this.state = {
-		bookings: [],
-	};
+		this.submitHandler = this.submitHandler.bind(this)
+		this.handleRoomNameChange = this.handleRoomNameChange.bind(this)
+		this.handleStartTimeChange = this.handleStartTimeChange.bind(this)
+		this.handleEndTimeChange = this.handleEndTimeChange.bind(this)
   }
 
-  componentDidMount() {
-	fetch('https://meetingrooms-booking.azurewebsites.net/bookings/findAll')
-		.then(response => response.json())
-		.then(data => this.setState({ bookings: data }));
+  // componentDidMount() {
+	// fetch('https://meetingrooms-booking.azurewebsites.net/bookings/findAll')
+	// 	.then(response => response.json())
+	// 	.then(data => this.setState({ bookings: data }));
+	// }
+
+	// let submit = document.querySelector("check-availability-form")
+
+	// submit.onClick()
+
+	submitHandler(event) {
+		console.log(this.state)
+		event.preventDefault()
 	}
 
+	handleRoomNameChange(event) {
+		this.setState({roomName: event.target.value}) 
+	}
+
+	handleStartTimeChange(event) {
+		this.setState({startTime: event.target.value}) 
+	}
+
+	handleEndTimeChange(event) {
+		this.setState({endTime: event.target.value}) 
+	}
+
+
+
   render() {
-	const { bookings } = this.state;
 
     return (
 	<div className="App">
@@ -28,26 +49,29 @@ class App extends Component {
 	  </header>
 
 		<p className="Intro"> To view availability please enter a room name alongside your preferred start and end times: </p>
-    <form className="check-availability-form">
+    <form className="check-availability-form" onSubmit={this.submitHandler}>
       <div>
-        <label for="room-name">Room name: </label>
-        <input type="text" name="room-name" placeholder="e.g. G05"></input>
+        <label htmlFor="room-name">Room name: </label>
+        <input type="text" name="room-name" placeholder="e.g. G05" onChange={this.handleRoomNameChange} required></input>
       </div>
 
       <div>
-        <label for="start-time">Start of booking: </label>
-        <input type="datetime-local" name="start-time"></input>
+        <label htmlFor="start-time">Start of booking: </label>
+        <input type="datetime-local" name="start-time" onChange={this.handleStartTimeChange} required></input>
       </div>
 
       <div>
-        <label for="room-name">End of booking: </label>
-        <input type="datetime-local" name="end-time"></input>
+        <label htmlFor="room-name">End of booking: </label>
+        <input type="datetime-local" name="end-time"onChange={this.handleEndTimeChange} required></input>
       </div>
 
       <div>
-        <button>Check availability</button>
+        <button type="submit">Check availability</button>
       </div>
     </form>
+
+		<div className="Availability">
+		</div>
 
 	</div>
     );
@@ -55,3 +79,7 @@ class App extends Component {
 }
 
 export default App;
+
+
+// Click button
+// Checks the Database using a function. Expect either a true or false return. Append 
