d97e8edab15999d7899c34d670c980cc771f3845
Chris Cooksley <christopher.cooksley@gmail.com>
2019-01-29 13:09:12 +0000

    Add form fields

--- src/App.js
+++ src/App.js
@@ -26,8 +26,28 @@ class App extends Component {
 	  <header className="App-header">
 	    <h4>Check Room Availability</h4>
 	  </header>
-		
-		<p className="Intro"> To view availability please enter a room name alongside your preferred start and end times: </p> 
+
+		<p className="Intro"> To view availability please enter a room name alongside your preferred start and end times: </p>
+    <form className="check-availability-form">
+      <div>
+        <label for="room-name">Room name: </label>
+        <input type="text" name="room-name" placeholder="e.g. G05"></input>
+      </div>
+
+      <div>
+        <label for="start-time">Start of booking: </label>
+        <input type="datetime-local" name="start-time"></input>
+      </div>
+
+      <div>
+        <label for="room-name">End of booking: </label>
+        <input type="datetime-local" name="end-time"></input>
+      </div>
+
+      <div>
+        <button>Check availability</button>
+      </div>
+    </form>
 
 	</div>
     );
